84dbf2fad6ab1fac2a67e72f88b25b53715547c3
Ajay_007 <kharatajay314@gmail.com>
2024-02-13 12:36:14 +0530

    updated url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     APIURL : 'https://jsonplaceholder.typicode.com',
-    baseURL : 'https://bacheror-technology.onrender.com/'
+    baseURL : 'https://bacheror-technology.onrender.com'
 };
